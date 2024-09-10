27978cf8736d0abd391bc4d36d12750d6e305386
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-09-10 19:32:39 +0700

    split app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:klontong/main.dart';
+import 'package:klontong/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
