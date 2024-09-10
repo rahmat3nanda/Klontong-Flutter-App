550a5b8c57222b12080f95a7afe28288df60aa3f
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-09-10 21:39:05 +0700

    Create helper

---
+++ lib/model/app/singleton_model.dart
@@ -0,0 +1,34 @@
+/*
+ * *
+ *  * singleton_model.dart - klontong
+ *  * Created by Rahmat Trinanda (rahmat3nanda@gmail.com) on 09/10/2024, 21:32
+ *  * Copyright (c) 2024 . All rights reserved.
+ *  * Last modified 07/13/2024, 21:22
+ *
+ */
+
+import 'package:flutter/material.dart';
+
+class SingletonModel {
+  static SingletonModel? _singleton;
+
+  factory SingletonModel() => _singleton ??= SingletonModel._internal();
+
+  SingletonModel._internal();
+
+  static SingletonModel withContext(BuildContext context) {
+    _singleton ??= SingletonModel._internal();
+    _singleton!.context = context;
+
+    return _singleton!;
+  }
+
+  static SingletonModel get shared => _singleton ??= SingletonModel._internal();
+
+  BuildContext? context;
+  String? token;
+
+  void destroy() {
+    _singleton = null;
+  }
+}
