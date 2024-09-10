9139a7fc2c589d41d7b975a09866f5e3b15f227e
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-09-10 21:39:27 +0700

    Create Home

---
+++ lib/page/home_page.dart
@@ -0,0 +1,24 @@
+/*
+ * *
+ *  * home_page.dart - klontong
+ *  * Created by Rahmat Trinanda (rahmat3nanda@gmail.com) on 09/10/2024, 21:17
+ *  * Copyright (c) 2024 . All rights reserved.
+ *  * Last modified 09/10/2024, 21:17
+ *  
+ */
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
