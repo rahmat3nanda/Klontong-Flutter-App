f96f151bcbeb5d1398237352ba238aa220462bf1
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-09-12 02:25:36 +0700

    implement auth bloc

--- lib/page/splash_page.dart
+++ lib/page/splash_page.dart
@@ -8,12 +8,14 @@
  */
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:klontong/bloc/auth/auth_bloc.dart';
 import 'package:klontong/common/constants.dart';
 import 'package:klontong/common/styles.dart';
 import 'package:klontong/model/app/singleton_model.dart';
-import 'package:klontong/page/home_page.dart';
+import 'package:klontong/page/main_page.dart';
 import 'package:klontong/tool/helper.dart';
 
 class SplashPage extends StatefulWidget {
@@ -25,7 +27,7 @@ class SplashPage extends StatefulWidget {
 
 class _SplashPageState extends State<SplashPage>
     with SingleTickerProviderStateMixin {
-  late SingletonModel _model;
+  late AuthBloc _bloc;
   late Helper _helper;
 
   late AnimationController _controller;
@@ -33,7 +35,8 @@ class _SplashPageState extends State<SplashPage>
   @override
   void initState() {
     super.initState();
-    _model = SingletonModel.withContext(context);
+    SingletonModel.withContext(context);
+    _bloc = BlocProvider.of<AuthBloc>(context);
     _helper = Helper();
     _controller = AnimationController(
       duration: const Duration(seconds: 2),
@@ -50,45 +53,59 @@ class _SplashPageState extends State<SplashPage>
 
   void _setup() async {
     await _controller.forward();
-    await Future.delayed(const Duration(seconds: 1));
-    _helper.moveToPage(_model.context!, page: const HomePage());
+    _bloc.add(const AuthLoadEvent());
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: FadeTransition(
-        opacity: CurvedAnimation(
-          parent: _controller,
-          curve: Curves.easeOut,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Transform.translate(
-              offset: const Offset(-12, 0),
-              child: SvgPicture.asset(
-                AppIcon.klontong,
-                width: 152,
+    return BlocListener(
+      bloc: _bloc,
+      listener: (c, s) {
+        if (s is AuthLoadSuccessState) {
+          _helper.moveToPage(context, page: const MainPage());
+        } else if (s is AuthLoadFailedState) {
+          _helper.moveToPage(context, page: const MainPage());
+        }
+      },
+      child: BlocBuilder(
+        bloc: _bloc,
+        builder: (c, s) {
+          return Scaffold(
+            body: FadeTransition(
+              opacity: CurvedAnimation(
+                parent: _controller,
+                curve: Curves.easeOut,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Transform.translate(
+                    offset: const Offset(-12, 0),
+                    child: SvgPicture.asset(
+                      AppIcon.klontong,
+                      width: 152,
+                    ),
+                  ),
+                  const SizedBox(height: 24, width: double.infinity),
+                  Text(
+                    "Klontong",
+                    style: TextStyle(
+                      color: AppColor.primaryLight,
+                      fontSize: 32,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    "Find your needs",
+                    style: GoogleFonts.parisienne(fontSize: 24),
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 24, width: double.infinity),
-            Text(
-              "Klontong",
-              style: TextStyle(
-                color: AppColor.primaryLight,
-                fontSize: 32,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              "Find your needs",
-              style: GoogleFonts.parisienne(fontSize: 24),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
